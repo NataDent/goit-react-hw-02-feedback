a73872475ef475a86e2a9b2e9aa64c1d25b1dea8
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-08-31 23:27:28 +0300

    App added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    this.setState(prevState => ({
+      [e.target.name]: prevState[e.target.name] + 1,
+    }));
+    this.countTotalFeedback();
+  };
+
+  countTotalFeedback = () =>
+    Object.values(this.state).reduce((acc, el) => acc + el);
+
+  countPositivePercentage = () =>
+    Math.round((this.state.good / this.countTotalFeedback()) * 100);
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              options={Object.keys(this.state)}
+              onClick={this.handlerClick}
+              countTotalFeedback={this.countTotalFeedback}
+              countPositiveFeedbackPercentage={this.countPositivePercentage}
+            />
+          ) : (
+            <Notification text="There is no feedback" />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StatisticsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const StatisticsText = styled.p`
+  font-size: 18px;
+`;
